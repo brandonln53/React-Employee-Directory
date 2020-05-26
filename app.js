86fd2d7430533f80a3ea88df22e039d08536d1cd
brandonln53 <brandonln@gmail.com>
2020-05-26 17:30:37 -0600

    heroku modifications; getting errors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express')
 const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000 
+app.use(express.static(publicPath));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.listen(port, () => {
+   console.log('Server is up!');
+});
