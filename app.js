e1c214775bcfd3bf51c7a8b671f9e11b79885985
brandonln53 <brandonln@gmail.com>
2020-05-27 06:56:31 -0600

    modifications to get heroku deployment to work

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+const express = require('express')
 const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+const app = express()
+const port = process.env.PORT || 3000 
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+app.listen(port, () => console.log(`App is live on port ${port}!`))
---
+++ build/precache-manifest.be5b14511140d4882bab470724dae68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e71fd9747e2be3e76b852dba2926ceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3cd9d612725192f22aa",
+    "url": "/static/css/main.04a44b1c.chunk.css"
+  },
+  {
+    "revision": "0b4d5acbb82a90934bb7",
+    "url": "/static/js/2.8070267c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8070267c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cd9d612725192f22aa",
+    "url": "/static/js/main.77c87799.chunk.js"
+  },
+  {
+    "revision": "020c6ba40f3738eb96e5",
+    "url": "/static/js/runtime-main.285ed37d.js"
+  }
+]);
